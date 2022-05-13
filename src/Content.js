8abc22ddea5eba2a5e1d2b76a4760cff8415d609
Luie John Lumanta <luiejohn.24@gmail.com>
2022-05-14 07:57:00 +0800

    test

--- src/Content.js
+++ src/Content.js
@@ -1,42 +1,38 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
-const Content= () => {
-	const [x, setX]=useState(0)
-	const [y, setY]=useState(0)
-	const [int, setInt]=useState(0)
-	const names=['Bob','Kevin', 'Dave'];
-	
-	const handleNameChange =() =>{
-	
-		const intRandom = Math.floor(Math.random() * 3);
-		setInt(intRandom)
-	}
-	
-	const setCoordinates = e =>{
-		setX(e.clientX)
-		setY(e.clientY)
-		
-	}
-	
-	useEffect(()=>{
-		window.addEventListener("mousemove",setCoordinates )
-	},[])
-	
-		
-	
-/*	const handleClick = () => {
+const Content = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [int, setInt] = useState(0);
+  const names = ["Bob", "Kevin", "Dave"];
+
+  const handleNameChange = () => {
+    const intRandom = Math.floor(Math.random() * 3);
+    setInt(intRandom);
+  };
+
+  const setCoordinates = (e) => {
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", setCoordinates);
+  }, []);
+
+  /*	const handleClick = () => {
 		console.log('button Clicked')
 	}
 	*/
-	return(
-		<main>
-			<p>
-				x {x}
-				y{y}
-			</p>
-			<button onClick={()=>handleNameChange()} >Button</button>
-			<button onClick={()=>setCoordinates()} >Button</button>
-		</main>
-		);
+  return (
+    <main>
+      <h1>Joel cute</h1>
+      <p>
+        x {x}y{y}
+      </p>
+      <button onClick={() => handleNameChange()}>Button</button>
+      <button onClick={() => setCoordinates()}>Button</button>
+    </main>
+  );
 };
 export default Content;
